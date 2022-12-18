3baf4e75b892f3e59bbf07edfa34eb6498c4560d
muhamd <muhamedahmed9099@gmail.com>
2022-12-18 04:09:45 +0200

    version 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+import MemesData from "./MemesData";
+function App() {
+  
+
+  return (
+    <>
+      <Header />  
+      <Meme />
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+
+
+
+function Header(){
+    return(
+        <header className="header">
+            <img className="header--image" src="https://media.istockphoto.com/id/499870283/vector/trollface-internet-troll-3d-illustration.jpg?s=1024x1024&w=is&k=20&c=8r2s0szq2bOFmta4GpIZL0DZVZGDA4tjM4elWwOTjQ0=" />
+            <h2 className="header--title">Meme Generetor</h2>
+            <h4 className="header--project"> React Course - Project 3</h4>
+        </header>
+    )
+} 
+
+export default Header;
